0f0d2d4ddd6ccf7aa7662e7c7185f6302e95ee46
John Dave Decano <johndavedecano@gmail.com>
2020-07-31 20:41:14 +0800

    Added initial sql interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,124 +1,21 @@
-import { ConnectionOptions, Database, Schema, ITransaction } from "./interfaces";
-import mysql, { Connection } from "mysql";
-
-let db: Connection;
+import { Database, SqlOptions, ModelList } from './interfaces'
 
 class SqlDatabase implements Database {
-  constructor(private options: ConnectionOptions) {}
+  constructor(private options: SqlOptions) {}
 
   connect(): Promise<any> {
-    db = mysql.createConnection(this.options);
-
-    return new Promise((resolve: any, reject: any) => {
-      db.connect((err: any) => {
-        return err ? reject(err.stack) : resolve();
-      });
-    });
+    return Promise.resolve()
   }
 
   connection(logs: boolean): void {
-    return;
+    return
+  }
+
+  models(models: ModelList[]): void {
+    return
   }
 }
 
-function Sql(options: ConnectionOptions): SqlDatabase {
-  const sql = new SqlDatabase(options);
-  return sql;
+export function Sql(options: SqlOptions): SqlDatabase {
+  return new SqlDatabase(options)
 }
-
-function Query<T>(query: string, value?: any): Promise<T> {
-  return new Promise((resolve, reject) => {
-    const callback = (error: any, result: any) => (error ? reject(error) : resolve(result));
-    try {
-      if (value) {
-        db.query(query, value, callback);
-        return;
-      }
-      db.query(query, callback);
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
-
-function CreateDatabase<T>(name: string): Promise<T> {
-  return Query<T>(`CREATE DATABASE ${name}`);
-}
-
-function CreateTable<T>(name: string, fields: Schema): Promise<T> {
-  const query: string = Object.keys(fields)
-    .map(key => {
-      const { type, options } = fields[key];
-      return `${key} ${type} ${options ? options : ""}`;
-    })
-    .join();
-  return Query<T>(`CREATE TABLE ${name} (${query})`);
-}
-
-function Insert<T>(table: string, fields: { [name: string]: any }): Promise<T> {
-  const query = Object.keys(fields);
-  const values = query.map(key => `'${fields[key]}'`).join();
-  return Query<T>(`INSERT INTO ${table} (${query.join()}) VALUES (${values})`);
-}
-
-function Select<T>(options: { table: string; columns?: string[]; where?: string; join?: string; orderBy?: string; limit?: number } | string): Promise<T> {
-  let query = `SELECT ${options}`;
-  if (typeof options !== "string") {
-    const { columns, table, join = "", where = "", orderBy = "", limit = "" } = options;
-    query = `SELECT ${columns ? `${columns.join(`,`)}` : "*"} FROM ${table} ${join} ${where} ${limit ? `LIMIT ${limit}` : ""} ${
-      orderBy ? `ORDER BY ${orderBy}` : ""
-    }`;
-  }
-
-  return Query<T>(query);
-}
-
-function Delete<T>(table: string, condition: string): Promise<T> {
-  return Query<T>(`DELETE FROM ${table} WHERE ${condition}`);
-}
-
-function Update<T>(table: string, fields: { [name: string]: any }, where: { [name: string]: string }): Promise<T> {
-  const filedsKey = Object.keys(fields);
-  const whereKey = Object.keys(where);
-  const values = filedsKey.map(key => `'${fields[key]}'`).join();
-  const condition = whereKey.map((key: any) => `'${whereKey[key]}'`).join();
-  return Query<T>(`UPDATE ${table} SET ${values} WHERE ${condition}`);
-}
-
-// tslint:disable-next-line: max-classes-per-file
-class Transaction implements ITransaction {
-  begin<T>(): Promise<T> {
-    return new Promise((resolve, reject) => {
-      db.beginTransaction((error: any): void => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve();
-      });
-    });
-  }
-
-  commit<T>(): Promise<T> {
-    return new Promise((resolve, reject) => {
-      db.commit((error: any): void => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve();
-        db.end();
-      });
-    });
-  }
-
-  rollback<T>(): Promise<T> {
-    return new Promise(resolve => {
-      db.rollback((): void => {
-        resolve();
-      });
-    });
-  }
-}
-
-export { Query, Sql, Schema, CreateDatabase, CreateTable, Insert, Select, Delete, Update, Transaction };
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,26 +1,17 @@
-export interface ConnectionOptions {
-  host: string;
-  user: string;
-  password: string;
-  database?: string;
+export interface SqlOptions {
+  host: string
+  user: string
+  password: string
+  database?: string
+}
+
+export interface ModelList {
+  name: string
+  path: string
 }
 
 export interface Database {
-  connect: () => Promise<any>;
-  connection: (logs: boolean) => void;
-}
-
-export interface ITransaction {
-  begin: () => Promise<any>;
-  commit: () => Promise<any>;
-  rollback: () => Promise<any>;
-}
-
-export interface Schema {
-  [k: string]: Field;
-}
-
-interface Field {
-  type: string;
-  options?: string;
+  connect: () => Promise<any>
+  connection: (logs: boolean) => void
+  models: (array: ModelList[]) => void
 }
