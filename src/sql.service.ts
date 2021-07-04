11aa3cff03ef62dc9211abcea6299277b93b83bb
Mac Ignacio <mack_ignacio@yahoo.com>
2021-07-04 09:51:07 +0800

    FEAT: Add Sql Service

---
+++ src/sql.service.ts
@@ -0,0 +1,66 @@
+import { Services } from "@mayajs/router";
+import { Options, Sequelize } from "sequelize";
+import { SqlOptions, SqlDatabases, SqlConnection, SqlUriConnection, SchemaObject, SqlInstance } from "./interfaces";
+
+export class SqlServices extends Services {
+  private list: SqlDatabases;
+
+  constructor() {
+    super();
+    this.list = {};
+  }
+
+  async connect(name: string) {
+    const db = this.list[name];
+    try {
+      const instance = this.createDbInstance(db.options);
+      await instance.authenticate();
+      this.list[name].instance = instance;
+      console.log(`\x1b[32m[mayajs] ${name} is connected!\x1b[0m`);
+      return instance;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  set options(value: SqlOptions) {
+    const { name, ...rest } = value;
+    this.list[name] = { ...rest, instance: new Sequelize(), models: {} };
+  }
+
+  database(name: string): SqlInstance {
+    return this.list[name];
+  }
+
+  mapSchemas(name: string): void {
+    const db = this.list[name];
+    if (db.schemas && db.schemas?.length > 0) {
+      db.instance.sync({ alter: true });
+      db.schemas.map(({ name, schema, options = {} }: SchemaObject) => db.instance.define(name.toLocaleLowerCase(), schema, options));
+      db.models = db.instance.models;
+    }
+  }
+
+  /**
+   * Creates a Sequelize instance.
+   *
+   * @param  {SqlUriConnection | SqlConnection | Options | string} settings
+   * @returns Sequelize
+   */
+  private createDbInstance(settings?: SqlUriConnection | SqlConnection | Options | string): Sequelize {
+    const { uri, options = {} } = settings as SqlUriConnection;
+    options.logging = false;
+
+    if (uri) {
+      return new Sequelize(uri, options);
+    }
+
+    const { database, username, password = "" } = settings as SqlConnection;
+
+    if (database) {
+      return new Sequelize(database, username, password, options);
+    }
+
+    return new Sequelize(settings as Options);
+  }
+}
